c3128485d3bf019b28de04373099ae9ff1c0a6bb
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-11-08 22:54:27 -0300

    add form logic

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Form = () => {
-  return <h2>Form</h2>
+
+  const [user, setUser] = useState({
+    name: '',
+    flight: ''
+  });
+
+  const {name, flight} = user;
+
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+
+    const {name, flight} = user;
+    if (name.trim() === '' || flight.trim() === '') {
+      console.error('invalid form');
+      // TODO: handle error
+      return;
+    }
+
+    console.log(user);
+    // TODO: submit form (API)
+  }
+
+  return (
+    <Fragment>
+      <h2>Form</h2>
+      <form onSubmit={submitForm}>
+        <div>
+          <label>Nombre</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Ej: Juan Pérez"
+            onChange={handleChange}
+            value={name}
+          />
+        </div>
+        <div>
+          <label>Nº de vuelo</label>
+          <input
+            type="text"
+            name="flight"
+            placeholder="Ej: AR678"
+            maxLength="5"
+            onChange={handleChange}
+            value={flight}
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+          >
+            Guardar
+          </button>
+        </div>
+      </form>
+    </Fragment>
+  )
 };
 
 export default Form;
